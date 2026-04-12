d453696e54665ea427ea3419a70ab1e85adb0c4d
elliot richardson <ear78@aol.com>
2026-04-12 13:30:02 -0700

    init updated netlify /api routes to use the express router

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,26 +1,16 @@
-// const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
-import express from "express";
-import cors from "cors";
+import express, {Router} from "express";
 import serverless from "serverless-http";
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
 
 const api = express();
+const router = Router();
 
-// const app = express();
-api.use(cors());
-
-api.use(
-  "/graphql",
-  graphqlHTTP({
+router.get("/graphql", graphqlHTTP({
     schema,
     graphiql: true,
-  }),
-);
+  }));
 
-const PORT = process.env.PORT || 5001;
-api.listen(PORT, () => console.log(`Server started on ${PORT}`));
+api.use("/api/", router);
 
 export const handler = serverless(api);
